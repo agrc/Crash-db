36f5972bd336309c8e8d2bfefa6af9b5bc814052
steveoh <sgourley@utah.gov>
2015-09-30 13:37:16 -0600

    Release v1.0.0

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ class Tox(TestCommand):
 
 setup(
     name="crash-dbseeder",
-    version="0.2.0",
+    version="1.0.0",
     license="MIT",
     description="ETL Crash Data",
     long_description="",
