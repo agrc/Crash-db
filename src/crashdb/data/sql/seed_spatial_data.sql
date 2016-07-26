0c9cddbf420b4bd4dd0844e616cc7b9828c89a06
steveoh <sgourley@utah.gov>
2016-07-26 16:00:14 -0600

    seed web mercator

--- src/crashdb/data/sql/seed_spatial_data.sql
+++ src/crashdb/data/sql/seed_spatial_data.sql
@@ -1,2 +1,2 @@
 insert into CrashLocation (crash_id, Shape)
-values (-47, geometry::STGeomFromText('POINT (5225035.106177688, ymin: 4373832.359194187)', 3857))
+values (-47, geometry::STGeomFromText('POINT (5225035 4373832)', 3857))
